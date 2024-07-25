1abf8547447d398f627083c0f933509c23e1df2f
Valerie Candor <21-05038@g.batstate-u.edu.ph>
2024-07-25 23:55:15 +0800

    fix bugs

--- he_brew_app/lib/screens/favorite/favorite.dart
+++ he_brew_app/lib/screens/favorite/favorite.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:he_brew_app/theme.dart';
-=======
-import 'package:flutter/scheduler.dart';
-import 'package:he_brew_app/constants.dart';
->>>>>>> 740eb4bb15a86b9bf59ca392ba756c5530a50ab1
 import 'package:he_brew_app/provider/favorite_provider.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -14,46 +9,11 @@ class FavoriteScreen extends StatefulWidget {
   State<FavoriteScreen> createState() => _FavoriteScreenState();
 }
 
-class _FavoriteScreenState extends State<FavoriteScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<Offset> _slideAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Initialize the animation controller and slide animation
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 500),
-      vsync: this,
-    );
-
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(0, 1), // Start from below the screen
-      end: Offset.zero, // End at the original position
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
-
-    // Start the animation
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      _animationController.forward();
-    });
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
+class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
     final provider = FavoriteProvider.of(context);
     final finalList = provider.favorites;
-
     return Scaffold(
       backgroundColor: contentColor,
       appBar: AppBar(
@@ -64,97 +24,92 @@ class _FavoriteScreenState extends State<FavoriteScreen>
         ),
         centerTitle: true,
       ),
-      body: SlideTransition(
-        position: _slideAnimation,
-        child: Column(
-          children: [
-            Expanded(
+      body: Column(
+        children: [
+          Expanded(
               child: ListView.builder(
-                itemCount: finalList.length,
-                itemBuilder: (context, index) {
-                  final favoriteItems = finalList[index];
-                  return Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white,
-                          ),
-                          child: Row(
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(10),
-                                width: 85,
-                                height: 85,
-                                decoration: BoxDecoration(
-                                  color: contentColor,
-                                  borderRadius: BorderRadius.circular(20),
+                  itemCount: finalList.length,
+                  itemBuilder: (context, index) {
+                    final favoriteItems = finalList[index];
+                    return Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Container(
+                            padding: const EdgeInsets.all(10),
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  width: 85,
+                                  height: 85,
+                                  decoration: BoxDecoration(
+                                    color: contentColor,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Image.asset(favoriteItems.image),
                                 ),
-                                child: Image.asset(favoriteItems.image),
-                              ),
-                              const SizedBox(width: 10),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    favoriteItems.title,
-                                    style: const TextStyle(
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
+                                const SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      favoriteItems.title,
+                                      style: const TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 5),
-                                  Text(
-                                    favoriteItems.category,
-                                    style: TextStyle(
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey.shade400,
-                                      fontSize: 16,
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      favoriteItems.category,
+                                      style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey.shade400,
+                                        fontSize: 16,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    "₱${favoriteItems.price}.00",
-                                    style: const TextStyle(
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      "₱${favoriteItems.price}.00",
+                                      style: const TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              )
-                            ],
+                                  ],
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Positioned(
-                        top: 50,
-                        right: 35,
-                        child: GestureDetector(
-                          onTap: () {
-                            finalList.removeAt(index);
-                            setState(() {});
-                          },
-                          child: const Icon(
-                            Icons.delete,
-                            color: Colors.red,
-                            size: 25,
+                        Positioned(
+                          top: 50,
+                          right: 35,
+                          child: GestureDetector(
+                            onTap: () {
+                              finalList.removeAt(index);
+                              setState(() {});
+                            },
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                              size: 25,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+                      ],
+                    );
+                  }))
+        ],
       ),
     );
   }
